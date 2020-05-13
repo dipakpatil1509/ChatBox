5d0bd65ed75cf898f1749ca7050b2bc18bffbd1c
dipakpatil1509 <60445189+dipakpatil1509@users.noreply.github.com>
2020-05-13 16:50:18 +0530

    Update urls.py

--- WhatsBook/WhatsBook/urls.py
+++ WhatsBook/WhatsBook/urls.py
@@ -36,11 +36,3 @@ urlpatterns = [
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
-
