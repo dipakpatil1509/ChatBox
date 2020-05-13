a990f22234685f8597cbda3a2036180698bffda6
dipakpatil1509 <60445189+dipakpatil1509@users.noreply.github.com>
2020-05-13 16:46:44 +0530

    Update settings.py

--- WhatsBook/WhatsBook/settings.py
+++ WhatsBook/WhatsBook/settings.py
@@ -155,5 +155,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'poojapatil7142@gmail.com'
-EMAIL_HOST_PASSWORD = '8007460875.'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
